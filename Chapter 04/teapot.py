18221d906e2fec171e631a76bf50413fd52af306
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2020-04-12 10:41:25 +0200

    Fixed some problems caused by map which in Python 3 is lazy

--- Chapter 04/teapot.py
+++ Chapter 04/teapot.py
@@ -1,25 +1,29 @@
 with open("teapot.off") as f:
     lines = f.readlines()
 
-vertex_count, face_count, edge_count = map(int,lines[1].split())
+vertex_count, face_count, edge_count = map(int, lines[1].split())
+
 
 def triple(xs):
-    return (xs[0],xs[1],xs[2])
+    return xs[0], xs[1], xs[2]
+
 
 def load_vertices():
     vertices = []
-    for i in range(2,2+vertex_count):
-        v = triple(map(float,lines[i].split()))
+    for i in range(2, 2 + vertex_count):
+        v = triple([float(v) for v in lines[i].split()])
         vertices.append(v)
     return vertices
+
 
 def load_polygons():
     polys = []
     vertices = load_vertices()
-    for i in range(2+vertex_count,2+vertex_count+face_count):
-        poly = map(vertices.__getitem__, map(int,lines[i].split()[1:]))
+    for i in range(2 + vertex_count, 2 + vertex_count + face_count):
+        poly = [vertices[int(v)] for v in lines[i].split()[1:]]
         polys.append(poly)
     return polys
+
 
 def triangulate(poly):
     if len(poly) < 3:
@@ -27,17 +31,18 @@ def triangulate(poly):
     # elif len(poly) == 3:
     #     return [poly]
     else:
-        for i in range(1,len(poly) - 1):
-            yield (poly[0], poly[i+1], poly[i])
+        for i in range(1, len(poly) - 1):
+            yield poly[0], poly[i + 1], poly[i]
+
 
 def load_triangles():
     tris = []
     polys = load_polygons()
     for poly in polys:
         for tri in triangulate(poly):
-            assert(len(tri)==3)
+            assert (len(tri) == 3)
             for v in tri:
-                assert(len(v)==3)
+                assert (len(v) == 3)
             tris.append(tri)
     return tris
 
