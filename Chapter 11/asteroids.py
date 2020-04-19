c11444f296ec3ab3db65496e2cef4f07038aa1da
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2020-04-19 16:53:44 +0200

    Reformatted code of chapter 10

--- Chapter 11/asteroids.py
+++ Chapter 11/asteroids.py
@@ -10,11 +10,12 @@ import numpy as np
 
 bounce = True
 
+
 class PolygonModel():
-    def __init__(self,points):
+    def __init__(self, points):
         self.points = points
         self.rotation_angle = 0
-        self.gravity = 0 # G * m1 from Newton's law
+        self.gravity = 0  # G * m1 from Newton's law
         self.mass = 1
         self.x = 0
         self.y = 0
@@ -25,47 +26,46 @@ class PolygonModel():
 
     def transformed(self):
         rotated = [vectors.rotate2d(self.rotation_angle, v) for v in self.points]
-        return [vectors.add((self.x,self.y),v) for v in rotated]
+        return [vectors.add((self.x, self.y), v) for v in rotated]
 
+    # def move(self, milliseconds, thrust_vector=(0,0), gravity_source):
+    #     tx, ty = thrust_vector
+    #     gx, gy = gravitational_field(src, self.x, self.y)
+    #     ax = tx + gx
+    #     ay = ty + gy
+    #     self.vx += ax * milliseconds/1000
+    #     self.vy += ay * milliseconds/1000
+    #
+    #     dx, dy = self.vx * milliseconds / 1000.0, self.vy * milliseconds / 1000.0
+    #     self.x, self.y = vectors.add((self.x,self.y), (dx,dy))
+    #
+    #     if bounce:
+    #         if self.x < -10 or self.x > 10:
+    #             self.vx = - self.vx
+    #         if self.y < -10 or self.y > 10:
+    #             self.vy = - self.vy
+    #     else:
+    #         if self.x < -10:
+    #             self.x += 20
+    #         if self.y < -10:
+    #             self.y += 20
+    #         if self.x > 10:
+    #             self.x -= 20
+    #         if self.y > 10:
+    #             self.y -=20
+    #
+    #     self.rotation_angle += self.angular_velocity * milliseconds / 1000.0
 
-# def move(self, milliseconds, thrust_vector=(0,0), gravity_source):
-#     tx, ty = thrust_vector
-#     gx, gy = gravitational_field(src, self.x, self.y)
-#     ax = tx + gx
-#     ay = ty + gy
-#     self.vx += ax * milliseconds/1000
-#     self.vy += ay * milliseconds/1000
-#
-#     dx, dy = self.vx * milliseconds / 1000.0, self.vy * milliseconds / 1000.0
-#     self.x, self.y = vectors.add((self.x,self.y), (dx,dy))
-#
-#     if bounce:
-#         if self.x < -10 or self.x > 10:
-#             self.vx = - self.vx
-#         if self.y < -10 or self.y > 10:
-#             self.vy = - self.vy
-#     else:
-#         if self.x < -10:
-#             self.x += 20
-#         if self.y < -10:
-#             self.y += 20
-#         if self.x > 10:
-#             self.x -= 20
-#         if self.y > 10:
-#             self.y -=20
-#
-#     self.rotation_angle += self.angular_velocity * milliseconds / 1000.0
-
-    def move(self, milliseconds, thrust_vector=(0,0), gravity_sources=[]):
+    def move(self, milliseconds, thrust_vector=(0, 0), gravity_sources=[]):
         ax, ay = thrust_vector
         gx, gy = gravitational_field(gravity_sources, self.x, self.y)
         ax += gx
         ay += gy
-        self.vx += ax * milliseconds/1000
-        self.vy += ay * milliseconds/1000
+        self.vx += ax * milliseconds / 1000
+        self.vy += ay * milliseconds / 1000
 
         dx, dy = self.vx * milliseconds / 1000.0, self.vy * milliseconds / 1000.0
-        self.x, self.y = vectors.add((self.x,self.y), (dx,dy))
+        self.x, self.y = vectors.add((self.x, self.y), (dx, dy))
 
         if bounce:
             if self.x < -10 or self.x > 10:
@@ -80,15 +80,15 @@ class PolygonModel():
             if self.x > 10:
                 self.x -= 20
             if self.y > 10:
-                self.y -=20
+                self.y -= 20
 
         self.rotation_angle += self.angular_velocity * milliseconds / 1000.0
 
     def segments(self):
         point_count = len(self.points)
         points = self.transformed()
-        return [(points[i], points[(i+1)%point_count])
-                for i in range(0,point_count)]
+        return [(points[i], points[(i + 1) % point_count])
+                for i in range(0, point_count)]
 
     def does_collide(self, other_poly):
         for other_segment in other_poly.segments():
@@ -98,52 +98,57 @@ class PolygonModel():
 
     def does_intersect(self, other_segment):
         for segment in self.segments():
-            if do_segments_intersect(other_segment,segment):
+            if do_segments_intersect(other_segment, segment):
                 return True
         return False
 
+
 class Ship(PolygonModel):
     def __init__(self):
-        super().__init__([(0.5,0), (-0.25,0.25), (-0.25,-0.25)])
+        super().__init__([(0.5, 0), (-0.25, 0.25), (-0.25, -0.25)])
 
     def laser_segment(self):
         dist = 20. * sqrt(2)
-        x,y = self.transformed()[0]
-        return (x,y), (x + dist * cos(self.rotation_angle), y + dist*sin(self.rotation_angle))
+        x, y = self.transformed()[0]
+        return (x, y), (x + dist * cos(self.rotation_angle), y + dist * sin(self.rotation_angle))
 
 
 class Asteroid(PolygonModel):
     def __init__(self):
-        sides = randint(5,9)
-        vs = [vectors.to_cartesian((uniform(0.5,1.0), 2 * pi * i / sides))
-                for i in range(0,sides)]
+        sides = randint(5, 9)
+        vs = [vectors.to_cartesian((uniform(0.5, 1.0), 2 * pi * i / sides))
+              for i in range(0, sides)]
         super().__init__(vs)
-        self.vx = uniform(-1,1)
-        self.vy = uniform(-1,1)
-        self.angular_velocity = uniform(-pi/2,pi/2)
+        self.vx = uniform(-1, 1)
+        self.vy = uniform(-1, 1)
+        self.angular_velocity = uniform(-pi / 2, pi / 2)
+
 
 class BlackHole(PolygonModel):
-    def __init__(self,gravity):
+    def __init__(self, gravity):
         vs = [vectors.to_cartesian((0.5, 2 * pi * i / 20))
-                for i in range(0,20)]
+              for i in range(0, 20)]
         super().__init__(vs)
         self.gravity = gravity
+
+
 # ASTEROID HELPERS
 
-def trajectory(start,end,steps):
-    xi,yi = start
-    xf,yf = end
-    xs = np.linspace(xi,xf,steps+1)
-    ys = np.linspace(yi,yf,steps+1)
+def trajectory(start, end, steps):
+    xi, yi = start
+    xf, yf = end
+    xs = np.linspace(xi, xf, steps + 1)
+    ys = np.linspace(yi, yf, steps + 1)
     model = Asteroid()
-    asts = [Asteroid() for _ in range(0,steps+1)]
-    for x,y,ast in zip(xs,ys,asts):
+    asts = [Asteroid() for _ in range(0, steps + 1)]
+    for x, y, ast in zip(xs, ys, asts):
         ast.vx = ast.vy = ast.angular_velocity = 0
         ast.points = model.points
         ast.x = x
         ast.y = y
         ast.draw_center = True
     return asts
+
 
 # INITIALIZE GAME STATE
 
@@ -153,7 +158,7 @@ ship.y = 3
 
 asteroid_count = 10
 
-default_asteroids = [Asteroid() for _ in range(0,asteroid_count)]
+default_asteroids = [Asteroid() for _ in range(0, asteroid_count)]
 
 black_hole = BlackHole(0.1)
 black_hole2 = BlackHole(0.1)
@@ -163,31 +168,35 @@ black_hole2.x, black_hole2.y = 2, -1
 
 black_holes = [black_hole, black_hole2]
 
+
 def gravitational_field(sources, x, y):
     fields = [vectors.scale(- source.gravity, (x - source.x, y - source.y))
-                for source in sources]
+              for source in sources]
     return vectors.add(*fields)
 
+
 for ast in default_asteroids:
-    ast.x = randint(-9,9) # 9 * uniform(ast.vy, 0)
-    ast.y = randint(-9,9) # 9 * uniform(0, - ast.vx)
+    ast.x = randint(-9, 9)  # 9 * uniform(ast.vy, 0)
+    ast.y = randint(-9, 9)  # 9 * uniform(0, - ast.vx)
 
 # HELPERS / SETTINGS
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-BLUE =  (0, 0, 255)
+BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
-RED =   (255, 0, 0)
-LIGHT_GRAY =  (240, 240, 240)
+RED = (255, 0, 0)
+LIGHT_GRAY = (240, 240, 240)
 DARK_GRAY = (128, 128, 128)
 width, height = 400, 400
 
-def to_pixels(x,y):
-    return (width/2 + width * x / 20, height/2 - height * y / 20)
+
+def to_pixels(x, y):
+    return (width / 2 + width * x / 20, height / 2 - height * y / 20)
+
 
 def draw_poly(screen, polygon_model, color=BLACK, fill=False):
-    pixel_points = [to_pixels(x,y) for x,y in polygon_model.transformed()]
+    pixel_points = [to_pixels(x, y) for x, y in polygon_model.transformed()]
     if fill:
         pygame.draw.polygon(screen, color, pixel_points, 0)
     else:
@@ -196,31 +205,32 @@ def draw_poly(screen, polygon_model, color=BLACK, fill=False):
         cx, cy = to_pixels(polygon_model.x, polygon_model.y)
         pygame.draw.circle(screen, BLACK, (int(cx), int(cy)), 4, 4)
 
-def draw_segment(screen, v1,v2,color=RED):
+
+def draw_segment(screen, v1, v2, color=RED):
     pygame.draw.line(screen, color, to_pixels(*v1), to_pixels(*v2), 2)
 
+
 def draw_grid(screen):
-    for x in range(-9,10):
-        draw_segment(screen, (x,-10), (x,10), color=LIGHT_GRAY)
-    for y in range(-9,10):
+    for x in range(-9, 10):
+        draw_segment(screen, (x, -10), (x, 10), color=LIGHT_GRAY)
+    for y in range(-9, 10):
         draw_segment(screen, (-10, y), (10, y), color=LIGHT_GRAY)
 
     draw_segment(screen, (-10, 0), (10, 0), color=DARK_GRAY)
     draw_segment(screen, (0, -10), (0, 10), color=DARK_GRAY)
 
 
-
 thrust = 3
 trajectory_mode = False
 trajectory_frame = 1000
 
+
 # INITIALIZE GAME ENGINE
 
 def main(asteroids=default_asteroids):
-
     pygame.init()
 
-    screen = pygame.display.set_mode([width,height])
+    screen = pygame.display.set_mode([width, height])
 
     pygame.display.set_caption("Asteroids!")
 
@@ -237,9 +247,9 @@ def main(asteroids=default_asteroids):
 
         clock.tick()
 
-        for event in pygame.event.get(): # User did something
-            if event.type == pygame.QUIT: # If user clicked close
-                done=True # Flag that we are done so we exit this loop
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
+                done = True  # Flag that we are done so we exit this loop
 
         # UPDATE THE GAME STATE
 
@@ -247,19 +257,19 @@ def main(asteroids=default_asteroids):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT]:
-            ship.rotation_angle += milliseconds * (2*pi / 1000)
+            ship.rotation_angle += milliseconds * (2 * pi / 1000)
 
         if keys[pygame.K_RIGHT]:
-            ship.rotation_angle -= milliseconds * (2*pi / 1000)
+            ship.rotation_angle -= milliseconds * (2 * pi / 1000)
 
         for ast in asteroids:
-            ast.move(milliseconds,(0,0),gravity_sources=black_holes)
+            ast.move(milliseconds, (0, 0), gravity_sources=black_holes)
 
         for bh in black_holes:
             others = [other for other in black_holes if other != bh]
-            bh.move(milliseconds, (0,0), others)
+            bh.move(milliseconds, (0, 0), others)
 
-        ship_thrust_vector = (0,0)
+        ship_thrust_vector = (0, 0)
 
         if keys[pygame.K_UP]:
             ship_thrust_vector = vectors.to_cartesian((thrust, ship.rotation_angle))
@@ -273,7 +283,6 @@ def main(asteroids=default_asteroids):
 
         # DRAW THE SCENE
 
-
         if not trajectory_mode:
             screen.fill(WHITE)
             draw_grid(screen)
@@ -284,7 +293,7 @@ def main(asteroids=default_asteroids):
         since_last_trajectory_frame += milliseconds
         if not trajectory_mode or since_last_trajectory_frame >= trajectory_frame:
 
-            draw_poly(screen,ship)
+            draw_poly(screen, ship)
             since_last_trajectory_frame = 0
 
             for bh in black_holes:
@@ -296,10 +305,10 @@ def main(asteroids=default_asteroids):
                 else:
                     draw_poly(screen, asteroid, color=GREEN)
 
-
         pygame.display.flip()
 
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
--- Chapter 11/draw3d.py
+++ Chapter 11/draw3d.py
@@ -8,16 +8,17 @@ from mpl_toolkits.mplot3d import Axes3D, proj3d
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from colors import *
 
+
 ## https://stackoverflow.com/a/22867877/1704140
 class FancyArrow3D(FancyArrowPatch):
     def __init__(self, xs, ys, zs, *args, **kwargs):
-        FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+        FancyArrowPatch.__init__(self, (0, 0), (0, 0), *args, **kwargs)
         self._verts3d = xs, ys, zs
 
     def draw(self, renderer):
         xs3d, ys3d, zs3d = self._verts3d
         xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
-        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
         FancyArrowPatch.draw(self, renderer)
 
 
@@ -26,16 +27,19 @@ class Polygon3D():
         self.vertices = vertices
         self.color = color
 
+
 class Points3D():
     def __init__(self, *vectors, color=black):
         self.vectors = list(vectors)
         self.color = color
 
+
 class Arrow3D():
-    def __init__(self, tip, tail=(0,0,0), color=red):
+    def __init__(self, tip, tail=(0, 0, 0), color=red):
         self.tip = tip
         self.tail = tail
         self.color = color
+
 
 class Segment3D():
     def __init__(self, start_point, end_point, color=blue):
@@ -43,9 +47,11 @@ class Segment3D():
         self.end_point = end_point
         self.color = color
 
+
 class Box3D():
     def __init__(self, *vector):
         self.vector = vector
+
 
 # helper function to extract all the vectors from a list of objects
 def extract_vectors_3D(objects):
@@ -67,83 +73,83 @@ def extract_vectors_3D(objects):
         else:
             raise TypeError("Unrecognized object: {}".format(object))
 
-def draw3d(*objects, origin=True, axes=True, width=6, save_as=None):
 
+def draw3d(*objects, origin=True, axes=True, width=6, save_as=None):
     fig = plt.gcf()
     ax = fig.add_subplot(111, projection='3d')
 
     all_vectors = list(extract_vectors_3D(objects))
     if origin:
-        all_vectors.append((0,0,0))
+        all_vectors.append((0, 0, 0))
     xs, ys, zs = zip(*all_vectors)
 
-    max_x, min_x = max(0,*xs), min(0,*xs)
-    max_y, min_y = max(0,*ys), min(0,*ys)
-    max_z, min_z = max(0,*zs), min(0,*zs)
+    max_x, min_x = max(0, *xs), min(0, *xs)
+    max_y, min_y = max(0, *ys), min(0, *ys)
+    max_z, min_z = max(0, *zs), min(0, *zs)
 
-    x_size = max_x-min_x
-    y_size = max_y-min_y
-    z_size = max_z-min_z
+    x_size = max_x - min_x
+    y_size = max_y - min_y
+    z_size = max_z - min_z
 
     padding_x = 0.05 * x_size if x_size else 1
     padding_y = 0.05 * y_size if y_size else 1
     padding_z = 0.05 * z_size if z_size else 1
 
-    plot_x_range = (min(min_x - padding_x,-2), max(max_x + padding_x,2))
-    plot_y_range = (min(min_y - padding_y,-2), max(max_y + padding_y,2))
-    plot_z_range = (min(min_z - padding_z,-2), max(max_z + padding_z,2))
+    plot_x_range = (min(min_x - padding_x, -2), max(max_x + padding_x, 2))
+    plot_y_range = (min(min_y - padding_y, -2), max(max_y + padding_y, 2))
+    plot_z_range = (min(min_z - padding_z, -2), max(max_z + padding_z, 2))
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
 
     def draw_segment(start, end, color=black, linestyle='solid'):
-        xs, ys, zs = [[start[i],end[i]] for i in range(0,3)]
+        xs, ys, zs = [[start[i], end[i]] for i in range(0, 3)]
         ax.plot(xs, ys, zs, color=color, linestyle=linestyle)
 
     if axes:
-        draw_segment((plot_x_range[0],0,0), (plot_x_range[1],0,0))
-        draw_segment((0,plot_y_range[0],0), (0,plot_y_range[1],0))
-        draw_segment((0,0,plot_z_range[0]), (0,0,plot_z_range[1]))
+        draw_segment((plot_x_range[0], 0, 0), (plot_x_range[1], 0, 0))
+        draw_segment((0, plot_y_range[0], 0), (0, plot_y_range[1], 0))
+        draw_segment((0, 0, plot_z_range[0]), (0, 0, plot_z_range[1]))
 
     if origin:
-        ax.scatter([0],[0],[0], color='k', marker='x')
+        ax.scatter([0], [0], [0], color='k', marker='x')
 
     for object in objects:
         if type(object) == Points3D:
             xs, ys, zs = zip(*object.vectors)
-            ax.scatter(xs,ys,zs,color=object.color)
+            ax.scatter(xs, ys, zs, color=object.color)
 
         elif type(object) == Polygon3D:
-            for i in range(0,len(object.vertices)):
+            for i in range(0, len(object.vertices)):
                 draw_segment(
                     object.vertices[i],
-                    object.vertices[(i+1)%len(object.vertices)],
+                    object.vertices[(i + 1) % len(object.vertices)],
                     color=object.color)
 
         elif type(object) == Arrow3D:
             xs, ys, zs = zip(object.tail, object.tip)
-            a = FancyArrow3D(xs,ys,zs, mutation_scale=20,arrowstyle='-|>', color=object.color)
+            a = FancyArrow3D(xs, ys, zs, mutation_scale=20, arrowstyle='-|>', color=object.color)
             ax.add_artist(a)
 
         elif type(object) == Segment3D:
             draw_segment(object.start_point, object.end_point, color=object.color)
 
         elif type(object) == Box3D:
-            x,y,z = object.vector
-            kwargs = {'linestyle':'dashed', 'color':'gray'}
-            draw_segment((0,y,0),(x,y,0),**kwargs)
-            draw_segment((0,0,z),(0,y,z),**kwargs)
-            draw_segment((0,0,z),(x,0,z),**kwargs)
-            draw_segment((0,y,0),(0,y,z),**kwargs)
-            draw_segment((x,0,0),(x,y,0),**kwargs)
-            draw_segment((x,0,0),(x,0,z),**kwargs)
-            draw_segment((0,y,z),(x,y,z),**kwargs)
-            draw_segment((x,0,z),(x,y,z),**kwargs)
-            draw_segment((x,y,0),(x,y,z),**kwargs)
+            x, y, z = object.vector
+            kwargs = {'linestyle': 'dashed', 'color': 'gray'}
+            draw_segment((0, y, 0), (x, y, 0), **kwargs)
+            draw_segment((0, 0, z), (0, y, z), **kwargs)
+            draw_segment((0, 0, z), (x, 0, z), **kwargs)
+            draw_segment((0, y, 0), (0, y, z), **kwargs)
+            draw_segment((x, 0, 0), (x, y, 0), **kwargs)
+            draw_segment((x, 0, 0), (x, 0, z), **kwargs)
+            draw_segment((0, y, z), (x, y, z), **kwargs)
+            draw_segment((x, 0, z), (x, y, z), **kwargs)
+            draw_segment((x, y, 0), (x, y, z), **kwargs)
         else:
             raise TypeError("Unrecognized object: {}".format(object))
 
     if save_as:
-        plt.savefig("images/"+save_as)
+        plt.savefig("images/" + save_as)
 
     plt.show()
